0cfef1576906430647c77999ee392bc8d8af783b
Rubi <rubidemiraj@gmail.com>
2022-06-19 14:00:48 +0200

    We have added the entities

---
+++ src/main/java/sda/travelagency/model/Packages.java
@@ -0,0 +1,42 @@
+package sda.travelagency.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Table
+@Entity(name = "PACKAGES")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Packages {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "PackageId")
+    private Long packageid;
+
+    @Column(name = "DepartureTime")
+    private String departureTime;
+
+    @Column(name = "ReturnTime")
+    private String returnTime;
+
+    @Column(name = "DescriptionPack")
+    private String descriptionPack;
+
+    @Column(name = "HotelName")
+    private String hotelName;
+
+    @Column(name = "IndividualTrip")
+    private boolean individual;
+
+    @Column(name = "GuideTrip")
+    private boolean guideTrip;
+
+
+}
---
+++ src/main/java/sda/travelagency/model/States.java
@@ -0,0 +1,24 @@
+package sda.travelagency.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Table
+@Entity(name = "STATES")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class States {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "StateId")
+    private Long stateId;
+
+    @Column(name = "StateName")
+    private String stateName;
+}
