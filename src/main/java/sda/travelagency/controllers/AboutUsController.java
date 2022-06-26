47e28fbe243efca3bf5c8b4a86e27c1f8c6617db
Rubi <rubidemiraj@gmail.com>
2022-06-26 12:33:11 +0200

    Added index.html fragments

--- src/main/java/sda/travelagency/controllers/AboutUsController.java
+++ src/main/java/sda/travelagency/controllers/AboutUsController.java
@@ -1,9 +1,12 @@
 package sda.travelagency.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class AboutUsController {
-    @RequestMapping("/about-us")
+    @RequestMapping("/about")
     public String index() {
         return "about-us/index";
     }
